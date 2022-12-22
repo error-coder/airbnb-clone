ee831f5bf238785590445e3662e7e5b98bcc743d
errorcoder <kjw8971@naver.com>
2022-12-23 00:20:02 +0900

    GraphQL Code Challenge

--- common/permissions.py
+++ common/permissions.py
@@ -6,5 +6,5 @@ class OnlyLoggedIn(BasePermission):
 
     message = "You need to be logged in for this!" # message는 permission이 user를 차단했을때 보임
 
-    def has_permission(self, source: typing.Any, info: Info):
+    def has_permission(self, source: typing.Any, info: Info, **kwargs):
         return info.context.request.user.is_authenticated # user is authenticated가 True나 False를 반환해줌
--- config/schema.py
+++ config/schema.py
@@ -1,16 +1,17 @@
 import strawberry
 from rooms import schema as rooms_schema
 
+
 # typing은 코드에 주석을 추가할 수 있게 해줌
 
 
 @strawberry.type
-class Query(rooms_schema.Query): # Query가 rooms_schema.Query를 상속함
+class Query(rooms_schema.Query): # Query가 rooms_schema.Query 상속함
     pass
 
 @strawberry.type
-class Mutation:
+class Mutation(rooms_schema.Mutation):
     pass
     
 
-schema = strawberry.Schema(query=Query,) # mutation=Mutation,)
+schema = strawberry.Schema(query=Query, mutation=Mutation)
