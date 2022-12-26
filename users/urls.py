25273da1dc70d0213b362bb94ccb4ca7f1c31d8a
errorcoder <kjw8971@naver.com>
2022-12-26 17:42:14 +0900

    JWT Encode

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("log-in", views.LogIn.as_view()),
     path("log-out", views.LogOut.as_view()),
     path("token-login", obtain_auth_token), # username과 password를 보내면 token 반환
+    path("jwt-login", views.JWTLogin.as_view()),
     path("@<str:username>", views.PublicUser.as_view()), # 위 코드보다 먼저 쓰면 username을 me로 보기 때문에 에러 발생
 ]
