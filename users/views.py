499cf97b9d613943bbabfd1f8c8a4518ac365b0b
errorcoder <kjw8971@naver.com>
2023-01-19 00:00:14 +0900

    Access Token

--- users/views.py
+++ users/views.py
@@ -121,4 +121,7 @@ class GithubLogIn(APIView):
 
     def post(self, request):
         code = request.data.get("code")
-        
+        access_token = request.post(f"https://github.com/login/oauth/access_token?code={code}&client_id=7295d1136e095e687640&&client_secret={settings.GH_SECRET}", headers={"Accept": "application/json"},)
+        access_token = access_token.json().get('access_token')
+        user_data = requests.get("https://api.github.com/user", headers={"Authorization":f"Bearer {access_token}"},)
+        user_data = user_data.json()
