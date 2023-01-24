c09d15080691fe511b51676f98eb5b326ebf8dbe
errorcoder <kjw8971@naver.com>
2023-01-24 18:21:24 +0900

    File Upload - Backend

--- medias/views.py
+++ medias/views.py
@@ -1,8 +1,12 @@
+import requests
+from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_200_OK
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound, PermissionDenied
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 from .models import Photo
 
 class PhotoDetail(APIView):
@@ -23,3 +27,12 @@ class PhotoDetail(APIView):
         photo.delete()
         return Response(status=HTTP_200_OK)
 
+
+class GetUploadURL(APIView):
+
+    def post(self, request):
+        url = f"https://api.cloudflare.com/client/v4/accounts/{settings.CF_ID}/images/v2/direct_upload"
+        one_time_url = requests.post(url, headeres={"Authorization": f"Bearer {settings.CF_TOKEN}",},)
+        one_time_url = one_time_url.json()
+        result = one_time_url.get('result')
+        return Response({"id" : result.get("id"), "uploadURL" : result.get('uploadURL'),},)
